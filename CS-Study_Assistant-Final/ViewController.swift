dda34d839d0fbdb9703104eb67f3d7497a855636
Damien Lo <damienlo@cdnis.edu.hk>
2020-03-31 16:32:32 +0800

    Initial Commit

---
+++ CS-Study_Assistant-Final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS-Study_Assistant-Final
+//
+//  Created by Damien Lo on 31/3/2020.
+//  Copyright © 2020 Damien Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
